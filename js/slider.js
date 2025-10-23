a66acd265285054ae248971cb7f091aeb4125d78
vlsfsdfsdf <vlads3619@gmail.com>
2025-10-24 01:30:33 +0200

    my first commit

---
+++ js/slider.js
@@ -0,0 +1,19 @@
+const swiper = new Swiper(".swiper-container", {
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    1400: {
+      slidesPerView: 1,
+    },
+  },
+  pagination: {
+    el: ".pagination",
+    bulletClass: "pagination__button",
+    bulletActiveClass: "pagination__button--active",
+  },
+  navigation: {
+    nextEl: ".carousel-button.next",
+    prevEl: ".carousel-button.prev",
+  },
+});
